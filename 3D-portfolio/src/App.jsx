a7de499c50f6bfbad16d4b90f2b3271d2287684e
BoyanK95 <boyan_k_95@abv.bg>
2023-05-13 01:24:41 +0300

    feat(App): add react-router-dom and components to App.tsx root component
    import BrowserRouter and all components and add them to App.tsx root component

--- 3D-portfolio/src/App.jsx
+++ 3D-portfolio/src/App.jsx
@@ -1,12 +1,35 @@
-function App() {
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Hero from './components/Hero';
+import Loader from './components/Loader';
+import Teck from './components/Teck';
+import Work from './components/Work';
+import Contact from './components/Contact';
+import Feedback from './components/Feedback';
+import Experiance from './components/Experiance';
+import StarsCanvas from './components/StarsCanvas';
 
+const App = () => {
     return (
-        <>
-            <div>
-                <h3>3D Dev Portfolio</h3>
+        <BrowserRouter>
+            <div className='relative z-0 bg-primary'>
+                <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                    <Navbar />
+                    <Hero />
+                </div>
+                <About />
+                <Experiance />
+                <Teck />
+                <Work />
+                <Feedback />
+                <div className='relative z-0'>
+                    <Contact />
+                    <StarsCanvas />
+                </div>
             </div>
-        </>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
