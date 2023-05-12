b3995e06ebd6878e14eb40ceb60a73ce33d9a2ba
BoyanK95 <boyan_k_95@abv.bg>
2023-05-13 02:48:08 +0300

    fix(tailwind): fixed tailwind to work with vite
    npm install -D tailwindcss postcss autoprefixer
    npx tailwindcss init -p, to fix tailwind and vite issue

--- 3D-portfolio/src/components/Navbar.jsx
+++ 3D-portfolio/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
     return (
         <nav className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-primary`}>
-            <div className='w-full flex-justify-between items-center max-w-7xl mx-auto'>
+            <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
                 <Link
                     to='/'
                     className='flex-items-center gap-2'
@@ -19,8 +19,11 @@ const Navbar = () => {
                         window.scrollTo(0, 0);
                     }}
                 >
-                    <img style={{maxWidth: '20rem'}} src={boyanlogo} className='w-9 h-9 object-contain' />
+                    <img style={{ maxWidth: '20rem' }} src={boyanlogo} className='w-9 h-9 object-contain' />
                 </Link>
+                <p className='text-white text-[18px] font-bold cursor-pointer'>
+                    Boyan <span className='sm:blo hidden text-red-200'>Koychev</span>
+                </p>
             </div>
         </nav>
     );
