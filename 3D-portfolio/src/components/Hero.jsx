dc9dd7c20287269d7a0cd73a7efc0822e3a3e158
BoyanK95 <boyan_k_95@abv.bg>
2023-05-22 01:07:20 +0300

    feat(Hero): create motion ancor

--- 3D-portfolio/src/components/Hero.jsx
+++ 3D-portfolio/src/components/Hero.jsx
@@ -19,11 +19,28 @@ const Hero = () => {
                         Hi, my name is <span className='text-[#915eff]'>Boyan Koychev</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        I develop UI and UX,<br className='sm:block hidden'/> Web Applications and Websites
+                        I develop UI and UX,
+                        <br className='sm:block hidden' /> Web Applications and Websites
                     </p>
                 </div>
             </div>
             <ComputerCanvas />
+
+            <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+                <a href='#about'>
+                    <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                        <motion.dev 
+                            animate={{y: [0, 24, 0]}}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: 'loop'
+                            }}
+                            className='w-3 h-3 rounded-full bg-secondary mb-1'
+                        />
+                    </div>
+                </a>
+            </div>
         </section>
     );
 };
