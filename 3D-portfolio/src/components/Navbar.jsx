803b6e429b0f981ba976540e828aec701ad7c2bd
BoyanK95 <boyan_k_95@abv.bg>
2023-05-21 19:25:38 +0300

    fix(Navbar): fix tailwind styles

--- 3D-portfolio/src/components/Navbar.jsx
+++ 3D-portfolio/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <nav className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-primary`}>
+        <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
             <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
                 <Link
                     to='/'
@@ -21,8 +21,9 @@ const Navbar = () => {
                     }}
                 >
                     <img style={{ maxWidth: '20rem' }} src={boyanlogo} className='w-10 h-10 object-contain' />
-                    <p className='text-white text-[17px] font-bold object-contain'>
-                        Web <span className='sm:block hidden text-red-200'>| Portfolio</span>
+                    <p className='text-white text-[17px] font-bold 
+                    flex'>
+                        BoyanK &nbsp;<span className='sm:block hidden text-red-200'>| Portfolio</span>
                     </p>
                 </Link>
                 <ul className='list-none hidden sm:flex flex-row gap-10'>
