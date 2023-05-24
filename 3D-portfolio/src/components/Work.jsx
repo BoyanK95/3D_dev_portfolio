72c496b12d2b7f3a87c784172461fc4d38acffa1
BoyanK95 <boyan_k_95@abv.bg>
2023-05-24 17:27:12 +0300

    feat(Work): create ProjectCard and Work section of the page

--- 3D-portfolio/src/components/Work.jsx
+++ 3D-portfolio/src/components/Work.jsx
@@ -6,6 +6,7 @@ import { github } from '../assets';
 import { SectionWrapper } from './hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from './utils/motion';
+import ProjectCard from './ProjectCard';
 
 const Work = () => {
     return (
@@ -21,8 +22,12 @@ const Work = () => {
                     className='mt-3 max-w-3xl text-secondary text-[17px] leading-[30px]'
                 >
                     Following projects showcases my skills and experiance through real-world examples of my work. Each
-                    project is briefly described with links to the code repositories.
+                    project is briefly described with links to the code repositories. I will soon be uploading more projects as I am working on several at the moment!
                 </motion.p>
+            </div>
+
+            <div className='mt-20 flex flex-wrap gap-7'>
+                {projects.map((project, index) => <ProjectCard key={`project-${index}`} index={index} {...project}/>)}
             </div>
         </>
     );
