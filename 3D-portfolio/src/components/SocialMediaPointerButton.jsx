97eb4f3ba09af84b8eef75f72647c89795f13c0e
BoyanK95 <boyan_k_95@abv.bg>
2024-11-09 00:22:08 +0200

    feat: create SocialMediaPointerButton

---
+++ 3D-portfolio/src/components/SocialMediaPointerButton.jsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+
+const SocialMediaPointerButton = () => {
+  return (
+    <Button
+      variant="default"
+      className="text-lg group transition-all duration-300 hover:-translate-y-1 hover:text-blue-200 hover:text-xl"
+    >
+      Write me on Social Media
+      <svg
+        className="w-10 h-10 transition-transform duration-300 group-hover:translate-y-1"
+        fill="none"
+        viewBox="0 0 18 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={3}
+          d="M19 14l-7 7m0 0l-7-7m7 7V3"
+        />
+      </svg>
+    </Button>
+  );
+};
+
+export default SocialMediaPointerButton;
