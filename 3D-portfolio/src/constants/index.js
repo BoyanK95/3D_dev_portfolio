95047d0539bf1ff7e20a80508c4c68194e498e25
BoyanK95 <boyan_k_95@abv.bg>
2025-08-27 17:09:59 +0300

    feat: add Cyber ark work exp

--- 3D-portfolio/src/constants/index.js
+++ 3D-portfolio/src/constants/index.js
@@ -135,19 +135,34 @@ const experiences = [
         ]
     },
     {
-        title: 'React-Typescript Developer',
+        title: 'Fullstack Developer',
         company_name: 'Yara International',
         icon: yara,
         iconBg: '#E6DEDD',
-        date: 'Feb 2023 - Present',
+        date: 'Feb 2023 - Oct 2025',
         points: [
-            'Developing and maintaining web applications using React.js and other related technologies.',
+            'Developing and maintaining web applications using React.js, Nest and other related technologies.',
+            'Maintaining and improving existing applications.',
             'Collaborating with worldwide diverse teams including designers, product managers, and other developers to create high-quality products.',
             'Implementing responsive design and ensuring cross-browser compatibility.',
             'Participating in code reviews and providing constructive feedback to other developers.'
         ]
     },
-    
+    {
+        title: 'Fullstack Developer`',
+        company_name: 'Cyber Ark',
+        icon: yara,
+        iconBg: '#E6DEDD',
+        date: 'Oct 2025 - Present',
+        points: [
+            'Developing and maintaining web applications using React, Golang, Java and other related technologies.',
+            'Collaborating with worldwide diverse teams including designers, product managers, and other developers to create high-quality products.',
+            'Developing fast, secure and relaibale backend infrastrucute.',
+            'Implementing CI/CD pipelines.',
+            'Implementing responsive design and ensuring cross-browser compatibility.',
+            'Participating in code reviews and providing constructive feedback to other developers.'
+        ]
+    }
 ];
 
 const testimonials = [
@@ -170,15 +185,14 @@ const testimonials = [
         testimonial: "After Boyan optimized our website, our traffic increased by 50%. We can't thank them enough!",
         name: 'Denis Kanh',
         designation: 'Custumer Support',
-        company: 'Crypto.com',
+        company: 'Crypto.com'
     }
 ];
 
 const projects = [
     {
         name: 'MeetUp App',
-        description:
-            'Single-page application platform created with NextJS that allows users to create a meet up.',
+        description: 'Single-page application platform created with NextJS that allows users to create a meet up.',
         tags: [
             {
                 name: 'nextjs',
